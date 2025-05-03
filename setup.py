4bc1f4e4f8084b88b3b290cc61de8637f72c0ab5
mmckerns@gmail.com <mmckerns@gmail.com>
2025-05-03 00:24:29 -0400

    update license metadata

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup_kwds = dict(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License-Expression :: OSI Approved :: BSD-3-Clause',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
